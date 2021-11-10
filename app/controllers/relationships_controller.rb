9229d8d3d9b122c793d465a12b65c8251a07e510
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-10 09:53:23 +0000

    [Fix]フォローボタン非同期通信化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,10 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.active_relationships.create(followed_id: params[:id])
-    redirect_to request.referer
   end
 
   def destroy
     current_user.active_relationships.find_by(followed_id: params[:id]).destroy
-    redirect_to request.referer
   end
 
   # jsで切り替えるならどちらか一つのアクションで良さそう？
