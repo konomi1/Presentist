8035510a677ade8760c148bbcc74ac0b8bafc388
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-10 09:47:20 +0000

    [Add]show画面にフォローボタン追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,9 +3,13 @@ class RelationshipsController < ApplicationController
   before_action :set_user
 
   def create
+    current_user.active_relationships.create(followed_id: params[:id])
+    redirect_to request.referer
   end
 
   def destroy
+    current_user.active_relationships.find_by(followed_id: params[:id]).destroy
+    redirect_to request.referer
   end
 
   # jsで切り替えるならどちらか一つのアクションで良さそう？
