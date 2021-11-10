ee135cfd0c5e1ea81d6e22b6b561ee5609a5036c
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-10 08:33:59 +0000

    [Fix]贈り物詳細画面にコメント追加
    非同期通信も追加済み

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_present
 
   def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.present_id = @present.id
+    if @comment.save
+      render :comment
+    else
+      render :errors
+    end
   end
 
   def destroy
+    comment = @present.comments.find(params[:id])
+    comment.destroy
+    render :comment
   end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
+  def set_present
+    @present = Present.find(params[:present_id])
+  end
 end
