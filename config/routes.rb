934dbc07a9b6c660f98619366b3a4399b63c9118
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-10 04:51:12 +0000

    [Add]homesコントローラーとビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations',
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'homes#top'
+  get '/about' => 'homes#about'
 end
