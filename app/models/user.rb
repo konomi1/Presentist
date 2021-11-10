f9fba3b15d533cbc45ae0affc36bd39346b21d3d
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-10 04:41:51 +0000

    [Fix]devise関連を日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :birthday, presence: true
+  validates :gender, presence: true
+  validates :introduce, length: { maximum: 300 }
+
+  enum gender: {male: 0, female:1 }
+
+  attachment :image
 end
