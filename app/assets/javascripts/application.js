5c9a8f28d667e2d8b0816cf1a0d78ddf95ae96e5
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-13 06:10:01 +0000

    [Add]フォロー機能のタブを前ページのリンクで切り替えできるよう変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,30 @@
 /*global $*/
 /*global location $*/
 $(document).on('turbolinks:load',function () {
+// 前ページからのアンカーで表示タブを変更
+  //アンカーを取得
+  var hash = location.hash;
+  hash = (hash.match(/^#tab\d+$/) || [])[0];
 
+  //タブネームにアンカーを入れる
+  if($(hash).length){
+    var tabname = hash.slice(1) ;
+  } else{
+    var tabname = "tab1";
+  }
 
-  $('#tab-menu a').on('click', function(event) {
-    $("#tab-contents .tab").hide();
-    $("#tab-menu .active").removeClass("active");
+  //何番目のタブか
+  var tabno = $('.follow-tab li#' + tabname).index();
+
+  $('.follow-tab .panel').hide();
+  $('.follow-tab a').removeClass('active');
+  $('.follow-tab .panel').eq(tabno).show();
+  $('.follow-tab a').eq(tabno).addClass('active');
+
+// タブクリック
+  $('.follow-tab a').on('click', function(event) {
+    $(".follow-tab .panel").hide();
+    $(".follow-tab .active").removeClass("active");
     $(this).addClass("active");
     $($(this).attr("href")).show();
     event.preventDefault();
