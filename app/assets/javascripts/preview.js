884a35342bac55191b0a5f96375766b5b80739e2
konomi1 <89084813+konomi1@users.noreply.github.com>
2021-11-17 09:31:56 +0000

    [Fix]贈り物新規作成、編集画面のレイアウト修正

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -14,3 +14,15 @@ $(document).on('turbolinks:load', function(){
 });
 
 // ギフト画像プレビュー
+$(document).on('turbolinks:load', function(){
+  $('#present_item_image').on('change', function (event) {
+  // inputの画像を読み込む
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        $("#preview").attr('src', event.target.result);
+    };
+  // ファイルを表示
+    reader.readAsDataURL(file);
+  });
+});
